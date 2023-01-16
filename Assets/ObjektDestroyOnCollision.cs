3c794c08fd2d4e762cd511718fedb706d6c1229c
Sopatosk <lucashs@outlook.com>
2023-01-16 13:41:58 +0100

    Map(14)
    Ta-da

---
+++ Assets/ObjektDestroyOnCollision.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjektDestroyOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("BlockFake1"))
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
