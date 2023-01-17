fc6be1075a7a726b355df326539200ba1fdfcbab
AntonBaksa <anton.baksa@elev.ga.lbs.se>
2023-01-17 13:17:58 +0100

    skjengoisbgub

--- Assets/Scripts/Enemy3.cs
+++ Assets/Scripts/Enemy3.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class Enemy3 : MonoBehaviour
 {
-    public float range;
-
-    public Transform target;
-
-    bool detected = false;
-
-    Vector2 direction;
-
-    public GameObject enemy3Projectile;
-
-    public float fireRate;
-
-    float nextTimeToFire=0;
+      public float Range;
+    public Transform Target;
+    bool Detected = false;
+    Vector2 Direction;
+    public GameObject Gun;
+    public GameObject bullet;
+    public float FireRate;
+    float nextTimeToFire = 0;
+    public Transform Shootpoint;
+    public float Force;
 
     // Start is called before the first frame update
     void Start()
@@ -28,49 +25,93 @@ public class Enemy3 : MonoBehaviour
     void Update()
     {
 
-        Vector2 targetPos = target.position;
+        /* Vector2 targetPos = target.position;
 
-        direction = targetPos - (Vector2)transform.position;
+         direction = targetPos - (Vector2)transform.position;
 
-        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, direction, range);
+         RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, direction, range);
 
+         if (rayInfo)
+         {
+             if (rayInfo.collider.gameObject.tag == "Player")
+             {
+                 if (detected==false)
+                 {
+                     detected = true;
+                     AlarmLight.GetComponent<SpriteRenderer>().color = Color.red;
+
+                 }
+             }
+             else
+             {
+                 if (detected == true)
+                 {
+                     detected = false;
+                     AlarmLight.GetComponent<SpriteRenderer>().color = Color.green;
+                 }
+             }
+         }
+
+         if (detected)
+         {
+             if(Time.time>nextTimeToFire)
+             {
+                 nextTimeToFire = Time.time + 1 / fireRate;
+                 Shoot();
+             }
+         }
+
+         if(detected)
+         {
+             Throw.transform.up = direction;
+         }*/
+        Vector2 targetPos = Target.position;
+        Direction = targetPos - (Vector2)transform.position;
+        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, Direction, Range);
         if (rayInfo)
         {
             if (rayInfo.collider.gameObject.tag == "Player")
             {
-                if (detected==false)
+                if (Detected == false)
                 {
-                    detected = true;
-                    
+                    Detected = true;
                 }
             }
             else
             {
-                if (detected == true)
+                if (Detected == true)
                 {
-                    detected = false;
-                    
+                    Detected = false;
                 }
             }
         }
-
-        if (detected)
+        if (Detected)
         {
-            if(Time.time>nextTimeToFire)
+            Gun.transform.up = Direction;
+            if (Time.time > nextTimeToFire)
             {
-                nextTimeToFire = Time.time + 1 / fireRate;
-                Shoot();
+                nextTimeToFire = Time.time + 1 / FireRate;
+                shoot();
             }
         }
     }
 
-    void Shoot()
+    void shoot()
     {
+        /*  GameObject enemy3ProjectileIns = Instantiate(enemy3Projectile, shootPoint.position, Quaternion.identity);
+          enemy3ProjectileIns.GetComponent<Rigidbody2D>().AddForce(direction * force); */
+
+        GameObject BulletIns = Instantiate(bullet, Shootpoint.position, Quaternion.identity);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
 
     }
 
+    /* private void OnDrawGizmosSelected()
+     {
+         Gizmos.DrawWireSphere(transform.position, range);
+     }*/
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.DrawWireSphere(transform.position, Range);
     }
 }
