4d7e0a9b1c9e0b6bf34e40850e67c8d7337696a9
AntonBaksa <anton.baksa@elev.ga.lbs.se>
2023-02-01 10:46:20 +0100

    hippety hoppety

--- Assets/Scripts/Enemy3Projectile.cs
+++ Assets/Scripts/Enemy3Projectile.cs
@@ -6,11 +6,24 @@ public class Enemy3Projectile : MonoBehaviour
 {
     public PlayerHealth playerHealth;
 
+    public GameObject Player;
+    Rigidbody2D rb;
+    public float force;
+
     public int damage = 1;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         playerHealth = FindObjectOfType<PlayerHealth>();
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = Player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot+90);
     }
 
     // Update is called once per frame
