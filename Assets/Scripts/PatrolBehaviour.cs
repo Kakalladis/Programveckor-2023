ce41bfc44a6fa0d9ad90a3d6c3c490e23345c34b
AntonBaksa <anton.baksa@elev.ga.lbs.se>
2023-02-21 21:50:25 +0100

    Fixed enemy :)

---
+++ Assets/Scripts/PatrolBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolBehaviour : MonoBehaviour
+{
+
+    public float speed;
+    public float rayDist;
+    private bool moveRight;
+    public Transform groundDetect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundDetect.position, Vector2.down, rayDist);
+
+        if (groundCheck.collider == false)
+        {
+            if (moveRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                moveRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveRight = true;
+            }
+        }
+    }
+}
